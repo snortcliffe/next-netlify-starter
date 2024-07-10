23a090588919683318b907c9396a89b55d053ed6
snortcliffe <snortcliffe@bonuspluginsystems.co.uk>
2024-07-10 15:14:27 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,7 @@ export default function Home() {
       <main>
         <Header title="Project Furnishings" />
         <p className="description">
-    <img alt="" src="https://lh7-us.googleusercontent.com/bosZKOsPpsRq9q2rxOc1YqxZISBNmJqdYC5J4gZ4u8vKWO-XIKsDx2SRD8zKwzzpu6Zw06fGcbINBs4KDvFjywlfKXgmkmSEHVDGKYdWRA_oKDjLXCyB_nmo-WB42iCYRmOStILuqPi-q1Di7A_NXCM" style="width: 621.33px; height: 349.50px; margin-left: 0.00px; margin-top: 0.00px; transform: rotate(0.00rad) translateZ(0px); -webkit-transform: rotate(0.00rad) translateZ(0px);" title="">
-          <code> website coming soon, contact 01482 373737  </code>
+              <code> website coming soon, contact 01482 373737  </code>
                </p>
       </main>
 
